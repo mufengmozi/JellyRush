b781077f50626c45e70eb099b5c19ebadff88741
mufengmozi <276355049@qq.com>
2015-12-01 22:26:05 +0800

    newop
    add opening scene

---
+++ Assets/Open/resources/script/MainContral.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class MainContral : MonoBehaviour {
+
+   
+	// Use this for initialization
+	void Start () {
+	
+    }
+	
+	// Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Option()
+    {
+        GameObject Can = GameObject.Find("Canvas");
+        Button bu = Can.GetComponentInChildren<Button>();
+    }
+}
